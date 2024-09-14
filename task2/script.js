1e0f7558e6f99cfd4e1036ab1c6d3c4414895682
Dexight <tupikin@sfedu.ru>
2024-09-14 18:21:29 +0300

    Histogram (todo change colors)

--- task2/script.js
+++ task2/script.js
@@ -7,8 +7,29 @@ canvas.style.width = "1400px";
 var ctx = canvas.getContext('2d');
 var imgData;
 
+// Для гистограммы
+var dictionaryR = []; 
+var dictionaryG = []; 
+var dictionaryB = [];
+
+//Обнуляем
+for (var i = 0; i < 256; i++) {
+    dictionaryR.push(0);
+    dictionaryG.push(0);
+    dictionaryB.push(0);
+}
+
 img.onload = function() {
 	default_img();
+    // Для гистограммы
+    var imgR = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var imgData = imgR.data;
+    for (var i = 0; i < imgData.length; i += 4) {
+        dictionaryR[imgData[i]]   += 1;
+        dictionaryG[imgData[i+1]] += 1;
+        dictionaryB[imgData[i+2]] += 1;
+    }
+    drawHistogram();
 };
 
 var default_img = function() {
@@ -66,3 +87,37 @@ document.getElementById('functionSelect').addEventListener('change', function() 
         default:        default_img();
     }
 });
+
+function drawHistogram() {
+    anychart.onDocumentReady(function() {
+        // Преобразуем прошлые данные в новые (для гистограммы - из строчного в столбцевое представление)
+        var dataR = [];
+        var dataG = [];
+        var dataB = [];
+
+        for (var i = 0; i < 256; i++) {
+            dataR.push([i, dictionaryR[i]]);
+            dataG.push([i, dictionaryG[i]]);
+            dataB.push([i, dictionaryB[i]]);
+        }
+
+        // Создаем график
+        var chart = anychart.column();
+
+        var seriesB = chart.column(dataB);
+        seriesB.name("Blue");
+        
+        var seriesG = chart.column(dataG);
+        seriesG.name("Green");
+        
+        var seriesR = chart.column(dataR);
+        seriesR.name("Red");
+
+        chart.title("Гистограмма RGB");
+        chart.xAxis().title("Intensity");
+        chart.yAxis().title("Count");
+
+        chart.container("container");
+        chart.draw();
+    });
+}
