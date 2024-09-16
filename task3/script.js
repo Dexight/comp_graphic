435bc8eb45f6b25294d4550cfb525c521898b892
DmytryPan <92853484+DmytryPan@users.noreply.github.com>
2024-09-16 16:48:09 +0300

    Update script.js

--- task3/script.js
+++ task3/script.js
@@ -1,19 +1,19 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 let data;
+var img;
 
 document.getElementById('uploadImage').addEventListener('change', function(event) {
     var file = event.target.files[0];
     var reader = new FileReader();
 
     reader.onload = function(e){
-        var img = new Image();
+        img = new Image();
         img.onload = function(e){
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img,0,0);
-            data = ctx.getImageData(0,0,canvas.width, canvas.height);
-            // updateCanvas(); //TODO
+            updateCanvas(); 
         }
         img.src = e.target.result;
     }
@@ -21,9 +21,9 @@ document.getElementById('uploadImage').addEventListener('change', function(event
 
 });
 
-// document.getElementById('hue').addEventListener('input', updateCanvas);
-// document.getElementById('saturation').addEventListener('input', updateCanvas);
-// document.getElementById('brightness').addEventListener('input', updateCanvas);
+document.getElementById('hue').addEventListener('input', updateCanvas);
+document.getElementById('saturation').addEventListener('input', updateCanvas);
+document.getElementById('brightness').addEventListener('input', updateCanvas);
 
 function rgb2hsv(r,g,b){
     r /= 255;
@@ -52,6 +52,60 @@ function rgb2hsv(r,g,b){
     return [h,s,v]
 }
 
-// function hsv2rgb(h,s,v){
+function hsv2rgb(h,s,v){
+    let r,g,b;
+    i = Math.floor(h/60)%6;
+    f = h/60 - Math.floor(h/60);
+    p = v*(1-s);
+    q = v*(1-f*s);
+    t = v*(1-(1-f)*s);
 
-// }
+    switch(i){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break; 
+        case 3: r = p, g = q, b = v; break; 
+        case 4: r = t, g = p, b = v; break; 
+        case 5: r = v, g = p, b = q; break; 
+    }
+    return [r * 255, g * 255, b * 255];
+}
+
+function updateCanvas() {
+    ctx.drawImage(img,0,0);
+    data = ctx.getImageData(0, 0, img.width, img.height);
+
+    console.log(data);
+    
+    const hue = parseInt(document.getElementById('hue').value);
+    const saturation = parseInt(document.getElementById('saturation').value) / 100;
+    const brightness = parseInt(document.getElementById('brightness').value) / 100;
+
+    document.getElementById('hueValue').textContent = hue;
+    document.getElementById('saturationValue').textContent = Math.round(saturation * 100);
+    document.getElementById('brightnessValue').textContent = Math.round(brightness * 100);
+
+    const newData = ctx.createImageData(data.width, data.height);
+
+    for (let i = 0; i < data.data.length; i += 4) {
+        let r = data.data[i];
+        let g = data.data[i + 1];
+        let b = data.data[i + 2];
+
+        let [h, s, v] = rgb2hsv(r, g, b);
+        // console.log(h,s,v);
+
+        h = hue;
+        s = saturation;
+        v = brightness;
+
+        [r, g, b] = hsv2rgb(h, s, v);
+
+        newData.data[i] = r;
+        newData.data[i + 1] = g;
+        newData.data[i + 2] = b;
+        newData.data[i + 3] = data.data[i + 3];
+    }
+
+    ctx.putImageData(newData, 0, 0);
+}
