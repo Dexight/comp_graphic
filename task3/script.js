44b5775daef784ced788f182681d2faa22cf7599
DmytryPan <92853484+DmytryPan@users.noreply.github.com>
2024-09-16 12:21:15 +0300

    Update script.js

--- task3/script.js
+++ task3/script.js
@@ -29,15 +29,29 @@ function rgb2hsv(r,g,b){
     r /= 255;
     g /= 255;
     b /= 255;
-    let h,s,v;
     var mx = Math.max(r,g,b); // максимум для перевода
     var mn = Math.min(r,g,b); // минимум для перевода
-    if (mx === mn){
-        h = 0;
+    let h=0,s=0,v=mx;
+    var d = mx - mn;
+
+    //Считаем h
+    if (mx !== mn){
+        if(mx === r && g>=b)
+            h = 60 * (g-b)/d;
+        else if(mx == r && g < b )
+            h = 60 * (g-b)/d + 360;
+        else if(mx == g)
+            h = 60 * (b-r)/d + 120;
+        else 
+            h = 60 * (r-g)/d + 240;
     }
-    else{
-        var d = mx - mn;
-         
+    // считаем s
+    if(mx != 0){
+        s = 1 - mn/mx;
     }
-    
+    return [h,s,v]
 }
+
+// function hsv2rgb(h,s,v){
+
+// }
