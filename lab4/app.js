af1ab7d23cb3f04a1e5eff1c2928b0529ec3e7b7
Dexight <tupikin@sfedu.ru>
2024-09-28 00:01:48 +0300

    points array -> points set

--- lab4/app.js
+++ lab4/app.js
@@ -1,37 +1,44 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-let points = [];
+let points = new Set();
 let polygons = [];
 let dotRadius = 3;
 
 canvas.addEventListener('click', (e) => {
     let x = e.offsetX;
     let y = e.offsetY;
-    points.push({ x, y });
-    ctx.beginPath();
-    ctx.arc(x, y, dotRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'red'; 
-    ctx.fill();
+    let point = { x, y };
+
+    if (![...points].some(p => p.x === x && p.y === y)) {
+        points.add(point);
+        ctx.beginPath();
+        ctx.arc(x, y, dotRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+    }
+    console.log("points size = " + points.size);
 });
 
 document.getElementById('build-polygon').addEventListener('click', buildPolygon);
 
 function buildPolygon() {
-    if (points.length < 1) {
+    if (points.size < 1) {
         alert('Недостаточно точек для построения полигона');
         return;
     }
-    polygons.push(points);
-    drawPolygon(polygons[polygons.length - 1]);
-    
+    let polygonPoints = [...points];
+    polygons.push(polygonPoints);
+    drawPolygon(polygonPoints);
+
     // Перекрашиваем точки полигона в черный
-    for (let i = 0; i < points.length; i++) {
+    polygonPoints.forEach(point => {
         ctx.beginPath();
-        ctx.arc(points[i].x, points[i].y, dotRadius, 0, 2 * Math.PI);
-        ctx.fillStyle = 'black'; 
+        ctx.arc(point.x, point.y, dotRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'black';
         ctx.fill();
-    }
-    points = [];
+    });
+
+    points.clear();
 }
 
 function drawPolygon(polygon) {
@@ -41,33 +48,32 @@ function drawPolygon(polygon) {
         ctx.lineTo(polygon[i].x, polygon[i].y);
     }
     ctx.closePath();
-    ctx.strokeStyle = 'black'; 
+    ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
 document.getElementById('clear-scene').addEventListener('click', clearScene);
 
 function clearScene() {
-  points = [];
-  polygons = [];
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    points.clear();
+    polygons = [];
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 canvas.addEventListener('contextmenu', (event) => {
     const x = event.offsetX;
     const y = event.offsetY;
-  
-    // Проверяем, находится ли клик внутри границы точки
-    for (let i = 0; i < points.length; i++) {
-      const point = points[i];
-      if (Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2)) < dotRadius) {
-        points.splice(i, 1); // Если клик внутри границы, удаляем точку из массива points
-  
-        ctx.clearRect(point.x - dotRadius, point.y - dotRadius, 2 * dotRadius, 2 * dotRadius); // Очистка квадратом
-  
-        break;
-      }
+
+    // Ищем точку в Set
+    for (let point of points) {
+        if (Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2)) < dotRadius) {
+            points.delete(point); // Удаляем точку из Set
+
+            ctx.clearRect(point.x - dotRadius, point.y - dotRadius, 2 * dotRadius, 2 * dotRadius); // Очистка квадратом
+
+            break;
+        }
     }
-  
+
     event.preventDefault();
 });
