1dedd1dec225146ac53c32e371df0825aed70c7d
Dexight <tupikin@sfedu.ru>
2024-10-13 18:39:09 +0300

    final Bezier

--- lab5/task3/app.js
+++ lab5/task3/app.js
@@ -23,75 +23,74 @@ canvas.addEventListener('click', (e) => {
         ctx.fillStyle = (points.size === 1)? 'black' : 'red';
         ctx.fill();
 
-        if (points.size === 4)
-        {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            
-            drawBezier(curPoints[0], curPoints[1], curPoints[2], curPoints[3]);
-            
-            let i = 0;
-            plast = curPoints[0];
-            for(let p of curPoints)
-            {
-                // рисуем точки
-                ctx.beginPath();
-                ctx.arc(p[0], p[1], dotRadius, 0, 2 * Math.PI);
-                if (p === curPoints[0] || p === curPoints[curPoints.length-1]) ctx.fillStyle = 'black'
-                else ctx.fillStyle = 'red';
-                ctx.fill();
-
-                // показываем вектор изгиба
-                if(i % 2 !== 0)
-                {
-                    ctx.moveTo(plast[0], plast[1]);
-                    ctx.lineTo(p[0], p[1]);
-                    ctx.strokeStyle = 'pink';
-                    ctx.stroke();
-                }
-                plast = p;
-                i++;
-            }
-        }
-        else if (points.size > 4)
-        {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            buildBezye();
-            let i = 0;
-            plast = curPoints[0];
-            for(let p of curPoints)
-            {
-                // рисуем точки
-                ctx.beginPath();
-                ctx.arc(p[0], p[1], dotRadius, 0, 2 * Math.PI);
-                if (p === curPoints[0] || p === curPoints[curPoints.length-1]) ctx.fillStyle = 'black'
-                else ctx.fillStyle = 'red';
-                ctx.fill();
-
-                // показываем вектор изгиба
-                if(i % 2 !== 0)
-                {
-                    ctx.moveTo(plast[0], plast[1]);
-                    ctx.lineTo(p[0], p[1]);
-                    ctx.strokeStyle = 'pink';
-                    ctx.stroke();
-                }
-                plast = p;
-                i++;
-            }
-        }
+        Bezier();
     }
 });
 
-// Очистка сцены
-document.getElementById('clear-scene').addEventListener('click', clearScene);
-function clearScene() 
+function Bezier()//true если точек хватает/false если не хватает
 {
-    curPoints = [];
-    points.clear();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+    if (points.size === 4)
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        drawBezier(curPoints[0], curPoints[1], curPoints[2], curPoints[3]);
+        
+        let i = 0;
+        plast = curPoints[0];
+        for(let p of curPoints)
+        {
+            // рисуем точки
+            ctx.beginPath();
+            ctx.arc(p[0], p[1], dotRadius, 0, 2 * Math.PI);
+            if (p === curPoints[0] || p === curPoints[curPoints.length-1]) ctx.fillStyle = 'black'
+            else ctx.fillStyle = 'red';
+            ctx.fill();
 
-function buildBezye() 
+            // показываем вектор изгиба
+            if(i % 2 !== 0)
+            {
+                ctx.moveTo(plast[0], plast[1]);
+                ctx.lineTo(p[0], p[1]);
+                ctx.strokeStyle = 'pink';
+                ctx.stroke();
+            }
+            plast = p;
+            i++;
+        }
+        return true;
+    }
+    else if (points.size > 4)
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        buildBezier();
+        let i = 0;
+        plast = curPoints[0];
+        for(let p of curPoints)
+        {
+            // рисуем точки
+            ctx.beginPath();
+            ctx.arc(p[0], p[1], dotRadius, 0, 2 * Math.PI);
+            if (p === curPoints[0] || p === curPoints[curPoints.length-1]) ctx.fillStyle = 'black'
+            else ctx.fillStyle = 'red';
+            ctx.fill();
+
+            // показываем вектор изгиба
+            if(i % 2 !== 0)
+            {
+                ctx.moveTo(plast[0], plast[1]);
+                ctx.lineTo(p[0], p[1]);
+                ctx.strokeStyle = 'pink';
+                ctx.stroke();
+            }
+            plast = p;
+            i++;
+        }
+        return true;
+    }
+    else return false;
+};
+
+function buildBezier() 
 {
     let prev = curPoints[0];
     let next;
@@ -138,5 +137,85 @@ function drawBezier(p0, p1, p2, p3)
         ctx.strokeStyle = 'black';
         ctx.stroke();
         b_last = b;
+    }
+}
+
+// Очистка сцены
+document.getElementById('clear-scene').addEventListener('click', clearScene);
+function clearScene() 
+{
+    curPoints = [];
+    points.clear();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+let isDragging = false;
+let dragPoint = null;
+
+// Обработчик события начала перетаскивания
+canvas.addEventListener('mousedown', (e) => {
+    let x = e.offsetX;
+    let y = e.offsetY;
+    
+    // Проверяем, было ли нажатие на точку
+    for (let point of curPoints) {
+        let distance = Math.sqrt((x - point[0])**2 + (y - point[1])**2);
+        if (distance <= dotRadius) {
+            isDragging = true;
+            dragPoint = point;
+            break;
+        }
+    }
+});
+
+// Обработчик события перемещения мыши
+canvas.addEventListener('mousemove', (e) => {
+    if (isDragging) 
+    {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        
+        dragPoint[0] = x;
+        dragPoint[1] = y;
+        
+        redrawScene();
+    }
+});
+
+// Обработчик события окончания перетаскивания
+canvas.addEventListener('mouseup', () => {
+    isDragging = false;
+    dragPoint = null;
+});
+
+// Удаление точкек на пкм
+canvas.addEventListener('contextmenu', function(e) 
+{
+    e.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    //if ([...points].some(p => p[0] === x && p[1] === y))
+    curPoints = curPoints.filter(p => Math.sqrt((mouseX - p[0]) ** 2 + (mouseY - p[1]) ** 2) > dotRadius);
+    points = new Set(curPoints);
+    redrawScene();
+});
+
+// Перерисовка сцены после перемещения точки
+function redrawScene() 
+{   
+    if(Bezier() === false)
+    {
+        //просто рисуем оставшиеся точки
+        for (let p of curPoints)
+        {
+            ctx.beginPath();
+            ctx.arc(p[0], p[1], dotRadius, 0, 2 * Math.PI);
+            if (p === curPoints[0]) ctx.fillStyle = 'black'
+            else ctx.fillStyle = 'red';
+            ctx.fill();
+        }
     }
 }
